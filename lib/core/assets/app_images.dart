171fc2f861dc53912d9e85b64b1820c7c028eb9b
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-08-07 20:18:33 +0700

    Add image, vector assets and install flutter_svg library.

---
+++ lib/core/assets/app_images.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static const String basePath = 'assets/images/';
+
+  static const String introBackground = '${basePath}intro_background.png';
+  static const String chooseModeBackground =
+      '${basePath}choose_mode_background.png';
+  static const String authenticationBackground =
+      '${basePath}authentication_background.png';
+}
