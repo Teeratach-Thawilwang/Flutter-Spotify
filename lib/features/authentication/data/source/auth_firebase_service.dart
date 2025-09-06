d78a1c7914ac3890965bbfedc70ce726baab53d9
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-09-07 04:04:46 +0700

    Install cloud_firestore and use it to create user document on signup.

--- lib/features/authentication/data/source/auth_firebase_service.dart
+++ lib/features/authentication/data/source/auth_firebase_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
@@ -34,10 +35,15 @@ class AuthFirebaseServiceImpl extends AuthFirebaseService {
   @override
   Future<Either<String, String>> signup(SignupUsecaseParams params) async {
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      var data = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: params.email,
         password: params.password,
       );
+
+      FirebaseFirestore.instance.collection('Users').add({
+        'name': params.fullName,
+        'email': data.user?.email,
+      });
 
       return Right('Signup was successful.');
     } on FirebaseAuthException catch (e) {
