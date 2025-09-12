c00255a3ae3988208947a13a412e334ede3a16fc
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-09-12 18:32:41 +0700

    Add id and isFavorite parameters to song entity and song model.

--- lib/features/song/data/models/song_model.dart
+++ lib/features/song/data/models/song_model.dart
@@ -2,16 +2,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:spotify/features/song/domain/entities/song_entity.dart';
 
 class SongModel {
+  String? id;
   String? title;
   String? artist;
   num? duration;
   Timestamp? releaseDate;
+  bool? isFavorite;
 
   SongModel({
+    required this.id,
     required this.title,
     required this.artist,
     required this.duration,
     required this.releaseDate,
+    required this.isFavorite,
   });
 
   SongModel.fromJson(Map<String, dynamic> data) {
@@ -25,10 +29,12 @@ class SongModel {
 extension SongModelX on SongModel {
   SongEntity toEntity() {
     return SongEntity(
+      id: id!,
       title: title!,
       artist: artist!,
       duration: duration!,
       releaseDate: releaseDate!,
+      isFavorite: isFavorite!,
     );
   }
 }
--- lib/features/song/domain/entities/song_entity.dart
+++ lib/features/song/domain/entities/song_entity.dart
@@ -1,15 +1,37 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SongEntity {
+  final String id;
   final String title;
   final String artist;
   final num duration;
   final Timestamp releaseDate;
+  final bool isFavorite;
 
   SongEntity({
+    required this.id,
     required this.title,
     required this.artist,
     required this.duration,
     required this.releaseDate,
+    required this.isFavorite,
   });
+
+  SongEntity copyWith({
+    String? id,
+    String? title,
+    String? artist,
+    num? duration,
+    Timestamp? releaseDate,
+    bool? isFavorite,
+  }) {
+    return SongEntity(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      artist: artist ?? this.artist,
+      duration: duration ?? this.duration,
+      releaseDate: releaseDate ?? this.releaseDate,
+      isFavorite: isFavorite ?? this.isFavorite,
+    );
+  }
 }
