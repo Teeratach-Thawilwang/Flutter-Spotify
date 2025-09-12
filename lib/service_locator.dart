502fd277ba082dbb61e04ba8f2cacb85e584978b
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-09-12 08:38:23 +0700

    Register song repository and service.

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,9 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:spotify/features/authentication/data/repositories/auth_repository_impl.dart';
-import 'package:spotify/features/authentication/data/source/auth_firebase_service.dart';
+import 'package:spotify/features/authentication/data/sources/auth_firebase_service.dart';
 import 'package:spotify/features/authentication/domain/repositories/auth_repository.dart';
 import 'package:spotify/features/authentication/domain/usecases/signin_usecase.dart';
 import 'package:spotify/features/authentication/domain/usecases/signup_usecase.dart';
+import 'package:spotify/features/song/data/repositories/song_repository_impl.dart';
+import 'package:spotify/features/song/data/sources/song_firebase_service.dart';
+import 'package:spotify/features/song/domain/repositories/song_repository.dart';
+import 'package:spotify/features/song/domain/usecases/get_news_songs.dart';
+import 'package:spotify/features/song/domain/usecases/get_play_list.dart';
 
 final sl = GetIt.instance;
 
@@ -12,4 +17,9 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<AuthRepository>(AuthRepositoryImpl());
   sl.registerSingleton<SignupUsecase>(SignupUsecase());
   sl.registerSingleton<SigninUsecase>(SigninUsecase());
+
+  sl.registerSingleton<SongFirebaseService>(SongFirebaseServiceImpl());
+  sl.registerSingleton<SongRepository>(SongRepositoryImpl());
+  sl.registerSingleton<GetNewsSongsUsecase>(GetNewsSongsUsecase());
+  sl.registerSingleton<GetPlayListUsecase>(GetPlayListUsecase());
 }
