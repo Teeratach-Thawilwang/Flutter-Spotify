42ee63d74a0d8b3889b7d24a60283f83c28187c6
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-09-12 08:39:56 +0700

    Create app url file.

---
+++ lib/core/constants/app_urls.dart
@@ -0,0 +1,7 @@
+class AppUrls {
+  static const coverFirestorage =
+      'https://firebasestorage.googleapis.com/v0/b/flutter-spotify-5f565.firebasestorage.app/o/covers%2F';
+  static const songFirestorage =
+      'https://firebasestorage.googleapis.com/v0/b/flutter-spotify-5f565.firebasestorage.app/o/songs%2F';
+  static const mediaAlt = 'alt=media';
+}
