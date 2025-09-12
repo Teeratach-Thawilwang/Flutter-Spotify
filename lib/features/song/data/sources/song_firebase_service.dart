e4ad70b3e142205fe1bbba7ef844e59172399e4c
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-09-12 18:32:52 +0700

    Add type either and create addOrRemoveFavoriteSong and isFavoriteSong method.

--- lib/features/song/data/sources/song_firebase_service.dart
+++ lib/features/song/data/sources/song_firebase_service.dart
@@ -1,16 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:spotify/features/song/data/models/song_model.dart';
 import 'package:spotify/features/song/domain/entities/song_entity.dart';
 
 abstract class SongFirebaseService {
-  Future<Either> getNewsSongs();
-  Future<Either> getPlayList();
+  Future<Either<String, List<SongEntity>>> getNewSongs();
+  Future<Either<String, List<SongEntity>>> getPlayList();
+  Future<Either<String, bool>> addOrRemoveFavoriteSong(String songId);
+  Future<bool> isFavoriteSong(String songId);
 }
 
 class SongFirebaseServiceImpl extends SongFirebaseService {
   @override
-  Future<Either> getNewsSongs() async {
+  Future<Either<String, List<SongEntity>>> getNewSongs() async {
     try {
       List<SongEntity> songs = [];
       var data = await FirebaseFirestore.instance
@@ -20,6 +23,11 @@ class SongFirebaseServiceImpl extends SongFirebaseService {
           .get();
       for (var element in data.docs) {
         var songModel = SongModel.fromJson(element.data());
+        songModel.id = element.reference.id;
+
+        bool isFavorite = await isFavoriteSong(element.reference.id);
+        songModel.isFavorite = isFavorite;
+
         songs.add(songModel.toEntity());
       }
       return Right(songs);
@@ -29,21 +37,87 @@ class SongFirebaseServiceImpl extends SongFirebaseService {
   }
 
   @override
-  Future<Either> getPlayList() async {
+  Future<Either<String, List<SongEntity>>> getPlayList() async {
     try {
       List<SongEntity> songs = [];
       var data = await FirebaseFirestore.instance
           .collection('Songs')
           .orderBy('releaseDate', descending: true)
-          .limit(3)
+          .limit(10)
           .get();
       for (var element in data.docs) {
         var songModel = SongModel.fromJson(element.data());
+        songModel.id = element.reference.id;
+
+        bool isFavorite = await isFavoriteSong(element.reference.id);
+        songModel.isFavorite = isFavorite;
+
         songs.add(songModel.toEntity());
       }
       return Right(songs);
     } catch (e) {
       return Left('An error occured, Please try again.');
+    }
+  }
+
+  @override
+  Future<Either<String, bool>> addOrRemoveFavoriteSong(String songId) async {
+    try {
+      final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+      final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+      late bool isFavorite;
+
+      var user = firebaseAuth.currentUser;
+      String uid = user!.uid;
+
+      QuerySnapshot favoriteSong = await firebaseFirestore
+          .collection('Users')
+          .doc(uid)
+          .collection('Favorites')
+          .where('songId', isEqualTo: songId)
+          .get();
+
+      if (favoriteSong.docs.isNotEmpty) {
+        await favoriteSong.docs.first.reference.delete();
+        isFavorite = false;
+      } else {
+        await firebaseFirestore
+            .collection('Users')
+            .doc(uid)
+            .collection('Favorites')
+            .add({'songId': songId, 'createdAt': Timestamp.now()});
+        isFavorite = true;
+      }
+
+      return Right(isFavorite);
+    } catch (e) {
+      return Left('An error occured, Please try again.');
+    }
+  }
+
+  @override
+  Future<bool> isFavoriteSong(String songId) async {
+    try {
+      final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+      final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+
+      var user = firebaseAuth.currentUser;
+      String uid = user!.uid;
+
+      QuerySnapshot favoriteSong = await firebaseFirestore
+          .collection('Users')
+          .doc(uid)
+          .collection('Favorites')
+          .where('songId', isEqualTo: songId)
+          .get();
+
+      if (favoriteSong.docs.isNotEmpty) {
+        return true;
+      }
+
+      return false;
+    } catch (e) {
+      return false;
     }
   }
 }
