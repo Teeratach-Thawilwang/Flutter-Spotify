bca4f9e9c81956dbe06f8d8f761db41c9d61e8ff
ธีรธัช ถวิลหวัง <100046376+Teeratach-Thawilwang@users.noreply.github.com>
2025-09-16 11:22:47 +0700

    remove old file to use FavoriteSongToggle cubit and state instead.

--- lib/features/song/presentation/bloc/favorite_song_state.dart
+++
@@ -1,9 +0,0 @@
-abstract class FavoriteSongState {}
-
-class FavoriteSongInitial extends FavoriteSongState {}
-
-class FavoriteSongUpdated extends FavoriteSongState {
-  final bool isFavorite;
-
-  FavoriteSongUpdated({required this.isFavorite});
-}
